47b2219ad26ff21f31a4cf22cea9882ce4fb7144
Gustavo <gustavomav@gmail.com>
2023-05-27 22:51:13 -0300

    Add files via upload

---
+++ Aula_05/Ex02.py
@@ -0,0 +1,6 @@
+for numero in range(1, 11):
+    print(f"Tabuada do {numero}:")
+    for multiplicador in range(1, 11):
+        resultado = numero * multiplicador
+        print(f"{numero} x {multiplicador} = {resultado}")
+    print()  # Adiciona uma linha em branco entre as tabuadas
